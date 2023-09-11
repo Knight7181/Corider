1d0ac43da77ea24402a1401c62f5fee6ab8a669b
prem kumar <100278574+Knight7181@users.noreply.github.com>
2023-09-11 19:49:19 +0530

    changes in main.py

--- main.py
+++ main.py
@@ -15,38 +15,38 @@ api = Api(app)
 api.add_resource(Users, '/users', resource_class_kwargs={'user_repository': user_repository})
 api.add_resource(User, '/users/<string:user_id>', resource_class_kwargs={'user_repository': user_repository})
 
-#
-# # Get user_resource.py by ID
-# @app.route('/users/<string:user_id>', methods=['GET'])
-# def get_user_by_id(user_id):
-#     user = user_repository.get_user_by_id(ObjectId(user_id)) if ObjectId.is_valid(user_id) else None
-#     if user:
-#         return json.dumps(user, default=str), 200, {'content-type': 'application/json'}
-#     else:
-#         return json.dumps({"status": 404, "message": 'User Not Found'}), 404, {'content-type': 'application/json'}
-#
-#
-# # Update user_resource.py by ID
-# @app.route('/users/<string:user_id>', methods=['PUT'])
-# def update_user(user_id):
-#     updated_data = request.json
-#     result = user_repository.update_user(ObjectId(user_id), updated_data) if ObjectId.is_valid(user_id) else 0
-#     if result > 0:
-#         updated_user = user_repository.get_user_by_id(ObjectId(user_id))
-#         return json.dumps(updated_user, default=str), 200, {'content-type': 'application/json'}
-#     else:
-#         return json.dumps({"status": 404, "message": 'User not found'}), 404, {'content-type': 'application/json'}
-#
-#
-# # Delete user_resource.py by ID
-# @app.route('/users/<string:user_id>', methods=['DELETE'])
-# def delete_user(user_id):
-#     result = user_repository.delete_user(ObjectId(user_id)) if ObjectId.is_valid(user_id) else 0
-#     if result > 0:
-#         return json.dumps({"status": 204, "message": 'User Deleted'}), 204,
-#     else:
-#         return json.dumps({"status": 404, "message": 'User Not Found'}), 404, {'content-type': 'application/json'}
-#
+
+#Get user_resource.py by ID
+@app.route('/users/<string:user_id>', methods=['GET'])
+def get_user_by_id(user_id):
+     user = user_repository.get_user_by_id(ObjectId(user_id)) if ObjectId.is_valid(user_id) else None
+     if user:
+         return json.dumps(user, default=str), 200, {'content-type': 'application/json'}
+     else:
+         return json.dumps({"status": 404, "message": 'User Not Found'}), 404, {'content-type': 'application/json'}
+
+
+ # Update user_resource.py by ID
+@app.route('/users/<string:user_id>', methods=['PUT'])
+def update_user(user_id):
+     updated_data = request.json
+     result = user_repository.update_user(ObjectId(user_id), updated_data) if ObjectId.is_valid(user_id) else 0
+     if result > 0:
+         updated_user = user_repository.get_user_by_id(ObjectId(user_id))
+         return json.dumps(updated_user, default=str), 200, {'content-type': 'application/json'}
+     else:
+         return json.dumps({"status": 404, "message": 'User not found'}), 404, {'content-type': 'application/json'}
+
+
+ # Delete user_resource.py by ID
+@app.route('/users/<string:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+     result = user_repository.delete_user(ObjectId(user_id)) if ObjectId.is_valid(user_id) else 0
+     if result > 0:
+         return json.dumps({"status": 204, "message": 'User Deleted'}), 204,
+     else:
+         return json.dumps({"status": 404, "message": 'User Not Found'}), 404, {'content-type': 'application/json'}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
